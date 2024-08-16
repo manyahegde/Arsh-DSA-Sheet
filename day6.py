e8db654b35d9239b7588e4c8a9f8994ace3c40f9
manyahegde <107339033+manyahegde@users.noreply.github.com>
2024-08-16 17:31:01 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,12 @@
+# best time to buy and sell stocks
+def maxProfit(prices):
+    mincost=float('inf')
+    max_profit=0
+    for i in prices:
+        mincost=min(mincost, i)
+        max_profit=max(max_profit, i-mincost)
+    return max_profit
+prices=[7,6,4,3,1]
+print(maxProfit(prices))
+
+
