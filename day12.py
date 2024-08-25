14c4622c07f8aed63e423a054a3240c8bb228f4d
manyahegde <107339033+manyahegde@users.noreply.github.com>
2024-08-25 20:07:23 +0530

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,16 @@
+# no. subarrays whose sum equals k
+def subarraysum(nums, k):
+    prefixsum=0
+    d={0:1}
+    result=0
+    for num in nums:
+        prefixsum+=num
+        if prefixsum-k in d:
+            result+=d[prefixsum-k]
+        if prefixsum not in d:
+            d[prefixsum]=1
+        else:
+            d[prefixsum]+=1
+    return result
+print(subarraysum([1,2,-1,0,-2,2], 0))
+
